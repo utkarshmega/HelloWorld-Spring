37fc24bb704b6d693df7567351af6bc79f58936e
utkarshmega <utkarsheduc@gmail.com>
2020-11-25 18:57:01 +0530

    Hello world Application using Spring

---
+++ src/main/java/com/capgemini/helloworldspring/controller/HelloWorldController.java
@@ -0,0 +1,43 @@
+package com.capgemini.helloworldspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capgemini.helloworldspring.model.User;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+	@RequestMapping(value = {"", "/", "/home"})
+	public String sayHello() {
+		return "Hello World!";
+	}
+	
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + "!";
+	}
+	
+	@GetMapping("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName();
+	}
+	
+	@PutMapping("/put/{firstName}")
+	public String sayHelloPut(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+		return "Hello " + firstName + " " + lastName + "!";
+	}
+}
